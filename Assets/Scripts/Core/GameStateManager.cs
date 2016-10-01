2f03803ea0f8679969e2f423e82976b82c1806e2
Marcus Aromatorio <aromatoriom@gmail.com>
2016-10-01 00:55:11 -0400

    Add beginning for game and player state management
    This commit adds two major scripts to the core directory with early
    indication of implementation as stubs or commented description

---
+++ Assets/Scripts/Core/GameStateManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Typical large-scale game states where "idling" is normal gameplay
+public enum GameState
+{
+    idling,
+    initializing,
+    paused,
+    playerDeath,
+    victoryAchieved,
+    saving,
+    exiting
+}
+/*
+    The Game State Manager class handles the large scale game transitions
+    Transitions are dependent on in game events such as Initialize, Pausing, 
+    Player dying, Boss encounter, level completion, and Exiting or Loading different scenes
+*/
+public class GameStateManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameState m_State { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        m_State = GameState.initializing;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    switch (m_State)
+        {
+            case GameState.idling:
+                break;
+
+            case GameState.initializing:
+                break;
+
+            case GameState.paused:
+                break;
+
+            case GameState.playerDeath:
+                break;
+
+            case GameState.victoryAchieved:
+                break;
+
+            case GameState.saving:
+                break;
+
+            case GameState.exiting:
+                break;
+
+            default:
+                break;
+        }
+	}
+}
---
+++ Assets/Scripts/Core/PlayerStateManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+// General player states that are separated into an enumeration
+public enum PlayerState
+{
+    idling,
+    walking,
+    crouching,
+    crawling,
+    attacking,
+    crouchattacking,
+    jumping,
+    jumpattacking
+}
+
+/*
+    This class passively manages how the player changes state
+    The player's state is largely dependent on the cooperation with the PlayerController class
+    The state manager does not directly intervene in player action as such interventions are 
+    mainly under the jurisdiction of interface implementation
+    (I.e. the player dies not because its state manager reads health is zero, but how
+    the player implements the IKillable interface)
+*/
+public class PlayerStateManager : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerState m_State { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        m_State = PlayerState.idling;
+	}
+	
+}
